3db31a6fa633bd6d1f648c76b15d6e9e020420ec
AstrumMechanica <astrummechanica@gmail.com>
2023-11-16 14:52:40 -0500

    fixed a typo

--- asmek_authcogs/app_settings.py
+++ asmek_authcogs/app_settings.py
@@ -5,3 +5,14 @@ from django.apps import apps
 from django.conf import settings
 
 # put your app settings here
+ASMEK_AUTHCOGS_COGS = getattr(
+    settings,
+    "ASMEK_AUTHCOGS_COGS",
+    [
+        "asmek_authcogs.cogs.about",  # make sure to remove the about cog from aadiscordbot if using this
+        "asmek_authcogs.cogs.auth",
+        "asmek_authcogs.cogs.links",
+        "asmek_authcogs.cogs.siege",
+        "asmek_authcogs.cogs.hr",
+    ],
+)
