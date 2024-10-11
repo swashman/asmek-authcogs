4940a57649d786ca504378a377b618bb5362e402
swashman <jnelson2288@gmail.com>
2024-10-10 22:27:46 -0400

    fixing

--- testauth/settings/local.py
+++ testauth/settings/local.py
@@ -10,7 +10,7 @@ Test settings
 
 from .base import *
 
-PACKAGE = "example"
+PACKAGE = "asmek_authcogs"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
